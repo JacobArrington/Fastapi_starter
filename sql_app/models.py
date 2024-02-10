5a0b81907533313ee0caf0e83e0c10009a87fcc8
Jacob Arrington <Jacobsarrington@gmail.com>
2024-02-10 01:26:07 -0500

    fixed typo in sql_app file stucture

---
+++ sql_app/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True, nullable=False)
+    user_name = Column(String, unique=True, nullable=False)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    
+
+    
